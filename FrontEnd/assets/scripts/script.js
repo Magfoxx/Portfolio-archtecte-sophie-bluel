61fab749d533c138ea8025076ccebd641cc42bfe
Magfoxx <hamcha.martial@gmail.com>
2024-06-13 12:12:10 +0200

    Étape 1.1  Récupération et affichage des travaux

---
+++ FrontEnd/assets/scripts/script.js
@@ -0,0 +1,29 @@
+// console.log('test');
+// Récupération des travaux
+const reponse = await fetch("http://localhost:5678/api/works");
+const works = await reponse.json();
+// console.log(reponse);
+
+// // Fonction qui permet de récupérer les travaux depuis l'API
+// for (let i = 0; i < works.length; i++) {
+//   // console.log(i);
+//   const figure = works[i];
+//   // console.log(figure);
+
+// Récupération des éléments du DOM
+const divGallery = document.querySelector('.gallery');
+// Création d'une balise figure pour acceuillir chaques projets
+const worksElement = document.createElement('figure');
+
+// Création des balises
+const imageElement = document.createElement('img');
+imageElement.src = figure.imageUrl
+const titleElement = document.createElement('figcaption');
+titleElement.innerText = figure.title;
+
+// Rattacher la balise 'figure' avec la div 'gallery'
+divGallery.appendChild(worksElement);
+// Rattacher l'image et le titre à la balise figure
+worksElement.appendChild(imageElement);
+worksElement.appendChild(titleElement);
+}
