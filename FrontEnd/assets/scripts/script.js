41e4208b9f0b1133f08e3ded191e5b61e53ac337
Magfoxx <hamcha.martial@gmail.com>
2024-06-13 15:50:12 +0200

    Ajout des boutons

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -22,14 +22,51 @@ function genererWorks(works) {
     imageElement.src = figure.imageUrl
     const titleElement = document.createElement('figcaption');
     titleElement.innerText = figure.title;
+    // Ajout de la categoryID
+    const categoryIdElement = document.createElement("p");
+    categoryIdElement.innerText = figure.categoryId;
+    // console.log(categoryIdElement);
 
     // Rattacher la balise 'figure' avec la div 'gallery'
     divGallery.appendChild(worksElement);
-    // Rattacher l'image et le titre à la balise figure
+    // Rattacher l'image et le titre à la balise 'figure'
     worksElement.appendChild(imageElement);
     worksElement.appendChild(titleElement);
-
+    // Rattacher la categoryID à la balise 'figure'
+    worksElement.appendChild(categoryIdElement);
   }
 }
 // Appel de la fonction
 genererWorks(works);
+
+// ------ PARTIE FILTRES ------ //
+
+// Création des boutons
+// Récupération de l"élément du DOM 
+const divFilterCategory = document.querySelector('.category-menu');
+// console.log(FilterCategory);
+
+// Création des boutons
+// Création du bouton 'TOUS'
+const btnAll = document.createElement('button');
+btnAll.setAttribute("class", "btn-all");
+btnAll.textContent = 'Tous';
+// Création du bouton 'Objet'
+const btnObject = document.createElement('button');
+btnObject.setAttribute("class", "btn-objects");
+btnObject.textContent = 'Objets';
+// Création du bouton 'Appartements'
+const btnApartment = document.createElement('button');
+btnApartment.setAttribute("class", "btn-Apartment");
+btnApartment.textContent = 'Appartements';
+// Création du bouton 'Hotel & Restaurants'
+const btnHotelsRestaurant = document.createElement('button');
+btnHotelsRestaurant.setAttribute("class", "btn-hotels-and-restaurants");
+btnHotelsRestaurant.textContent = 'Hotels & restaurants';
+
+// Rattacher les boutons
+divFilterCategory.appendChild(btnAll);
+divFilterCategory.appendChild(btnObject);
+divFilterCategory.appendChild(btnApartment);
+divFilterCategory.appendChild(btnHotelsRestaurant);
+
