03143151ec71c5c8e25de8754bb2a0a933e11c19
Magfoxx <hamcha.martial@gmail.com>
2024-06-13 13:13:09 +0200

    Étape 1.1, Ajout de la fonction

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -4,26 +4,32 @@ const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 // console.log(reponse);
 
-// boucle qui permet de récupérer les travaux depuis l'API
-for (let i = 0; i < works.length; i++) {
-  // console.log(i);
-  const figure = works[i];
-  // console.log(figure);
+// fonction qui permet de récupérer les travaux depuis l'API
+function genererWorks(works) {
+  // boucle listant les projets
+  for (let i = 0; i < works.length; i++) {
+    // console.log(i);
+    const figure = works[i];
+    // console.log(figure);
 
-  // Récupération des éléments du DOM
-  const divGallery = document.querySelector('.gallery');
-  // Création d'une balise figure pour acceuillir chaques projets
-  const worksElement = document.createElement('figure');
+    // Récupération des éléments du DOM
+    const divGallery = document.querySelector('.gallery');
+    // Création d'une balise figure pour acceuillir chaques projets
+    const worksElement = document.createElement('figure');
 
-  // Création des balises
-  const imageElement = document.createElement('img');
-  imageElement.src = figure.imageUrl
-  const titleElement = document.createElement('figcaption');
-  titleElement.innerText = figure.title;
+    // Création des balises
+    const imageElement = document.createElement('img');
+    imageElement.src = figure.imageUrl
+    const titleElement = document.createElement('figcaption');
+    titleElement.innerText = figure.title;
 
-  // Rattacher la balise 'figure' avec la div 'gallery'
-  divGallery.appendChild(worksElement);
-  // Rattacher l'image et le titre à la balise figure
-  worksElement.appendChild(imageElement);
-  worksElement.appendChild(titleElement);
+    // Rattacher la balise 'figure' avec la div 'gallery'
+    divGallery.appendChild(worksElement);
+    // Rattacher l'image et le titre à la balise figure
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(titleElement);
+
+  }
 }
+// Appel de la fonction
+genererWorks(works);
