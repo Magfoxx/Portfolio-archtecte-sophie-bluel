6488796889474260d469a75dc2d5cf8f7a3ea58c
Magfoxx <hamcha.martial@gmail.com>
2024-06-20 18:08:43 +0200

    Étape 2.2 'Formulaire de connexion' Ajout des règles de validation

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,31 +1,52 @@
+// Récupération des éléments du DOM
 const form = document.querySelector("form");
-const submitButton = document.getElementById("btn-submit");
+const errorMessage = document.getElementById('error-message');
 
-// Pour vérifier si mon bouton submit est valide au click
-submitButton.addEventListener("click", function () {
-  // console.log("Le bouton 'Se connecter' a été cliqué !");
-});
+// Fonction de soumission du formulaire
 
-form.addEventListener("submit", async function (event) {
-  event.preventDefault(); // Empêche le formulaire de se soumettre pour démonstration
-  // alert("Le formulaire a été soumis !");
+async function handleSubmit(event) {
+  event.preventDefault(); // 'preventDefault()' empêche le rechargement de la page tandis que le 'handleSubmit'gère l’événement de soumission d’un formulaire
 
-  // Récupérer les valeurs des champs du formulaire
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const errorMessage = document.getElementById('erre-message')
-  // console.log('Email:', email);
-  // console.log('Password:', password);
+  const email = document.getElementById("email").value.trim(); // Rajout de 'trim()' pour supprimer les espaces blancs au début et à la fin d'un string  
+  const password = document.getElementById("password").value.trim();
 
+  // Vérification des champs
+  if (!email || !password) {
+    alert("Veuillez remplir tous les champs.");
+    return; // Si rien n'est remplie dans l'email ou le password alors une alerte apparaît
+  }
 
-  const reponseLogin = await fetch('http://localhost:5678/api/users/login', {
-    method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      email: email,
-      password: password
-    })
-  })
-  console.log('Connexion réussie', reponseLogin);
+  // Le bloc try contient le code qui pourrait potentiellement générer une erreur. Si une erreur se produit à n’importe quel endroit dans ce bloc, l’exécution du code dans le bloc try s’arrête immédiatement et le contrôle passe au bloc catch.
+  try {
+    const response = await fetch('http://localhost:5678/api/users/login', {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    });
 
-});
+    // Le 'if (response.ok)' est utilisé pour vérifier si la réponse obtenue de la requête fetch est valide et pour gérer les différents cas en fonction du statut de la réponse.
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error('Utilisateur non trouvé');
+      } else if (response.status === 401) {
+        throw new Error('Mot de passe incorrect');
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erreur lors de la connexion.');
+      }
+    }
+
+    const data = await response.json();
+
+    // Si la connexion est réussie, redirection !
+    window.location.href = "./index.html";
+    // affichage du login dans la console
+    console.log('Success', data);
+  } catch (error) {
+    // Affichage de l'erreur
+    errorMessage.textContent = error.message;
+  }
+}
+
+// Écouter l'événement de soumission du formulaire
+form.addEventListener("submit", handleSubmit);
