ef4a58425622367b7804d4e502873556d25f1860
Magfoxx <hamcha.martial@gmail.com>
2024-06-28 18:38:38 +0200

    mise à jour de la modale Première vue, Uniquement l'affichagevisuel

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -11,8 +11,10 @@ if (token) {
 }
 fetchData();
 
+// =============================================================================
+// ==== Fonction de récupération des travaux et des catégories depuis l'API ====
+// =============================================================================
 
-// Fonction de récupération des données depuis l'API
 async function fetchData() {
   try { // Récupération des travaux
     const reponseWorks = await fetch("http://localhost:5678/api/works");
@@ -37,86 +39,14 @@ async function fetchData() {
     console.error("Erreur lors de la récupération des données :");
   }
 }
-// fonction qui permet de récupérer la liste des travaux depuis l'API
-function genererWorks(listWorks) {
-  // Récupération des éléments du DOM
-  const divGallery = document.querySelector('.gallery');
-  if (!divGallery) {
-    console.error("L'élément .gallery n'existe pas sur cette page.");
-    return;
-  }
-  //Effacement de l'écran et regénération de la page  
-  divGallery.innerHTML = "";
-  // Boucle qui va permettre de lister les projets
-  for (let i = 0; i < listWorks.length; i++) {
-    const figure = listWorks[i];
-    // Création de la balise figure qui acceuillera chaque projet
-    const worksElement = document.createElement('figure');
-
-    // Création des balises
-    const imageElement = document.createElement('img');
-    imageElement.src = figure.imageUrl
-    const titleElement = document.createElement('figcaption');
-    titleElement.innerText = figure.title;
-    const categoryIdElement = document.createElement("p");
-    categoryIdElement.innerText = figure.categoryId;
-
-    // Rattacher les balises
-    divGallery.appendChild(worksElement);
-    worksElement.appendChild(imageElement);
-    worksElement.appendChild(titleElement);
-    worksElement.appendChild(categoryIdElement);
-  }
-}
-// ----------- gestion des boutons filtres -----------  
-
-function genererFilter(listCategory, listWorks) {
-
-  //   // Récupération de l'élément du DOM
-  const divFilterCategory = document.querySelector(".category-menu");
-  divFilterCategory.innerHTML = "";
-  // Création du bouton 'Tous' 
-  const buttonFilterAll = document.createElement('button');
-  buttonFilterAll.setAttribute('class', 'btn-tous');
-  buttonFilterAll.textContent = "Tous";
-  divFilterCategory.appendChild(buttonFilterAll);
-
-  // Boucle pour afficher le nom de chaques catégorie de l'API
-  for (let i = 0; i < listCategory.length; i++) {
-    // console.log(listCategory.length);
-    const buttonFilter = document.createElement('button');
-    // Récupération de l'id du bouton pour l'event Listener
-    buttonFilter.dataset.id = listCategory[i].id;
-    // console.log(buttonFilter.dataset.id) // Cela m'affiche bien mes 3 id
-    buttonFilter.setAttribute('class', 'btn-filter');
-    buttonFilter.innerText = listCategory[i].name
-    divFilterCategory.appendChild(buttonFilter);
-  }
-
-  // ----------- Ajout de l'event listener sur les boutons -----------  
-
-  // Pour le bouton 'Tous'
-  buttonFilterAll.addEventListener('click', () => {
-    genererWorks(listWorks);
-  })
-}
-// fonction pour les autres boutons
-function addListenerFilter(listWorks) {
-  const listButton = document.querySelectorAll('.btn-filter');
-  for (let i = 0; i < listButton.length; i++) {
-    const currentButton = listButton[i];
-    currentButton.addEventListener('click', (event) => {
-      const categoryId = parseInt(event.target.dataset.id);
-      const listWorksFilter = listWorks.filter(work => work.categoryId === categoryId);
-      genererWorks(listWorksFilter);
-    });
-  }
-}
+// =============================================================================
+// =========================== Fonction Mode Édition ===========================
+// =============================================================================
 
 function editMode() {
   const divFiltreCategories = document.querySelector(".category-menu");
   if (divFiltreCategories) {
-    divFiltreCategories.style.display = "none";// La section des filtres est caché
+    divFiltreCategories.style.display = "none";// La section des boutons filtre est caché
   }
   // Remplacement de 'login' par 'logout'
   const loginButton = document.getElementById("btn-Login");
@@ -128,9 +58,6 @@ function editMode() {
       window.Location.href = "login.html"; // redirection vers login.html au click
     })
   }
-
-  // ----------- Mode Édition & Classique ----------- 
-
   // Vérifier si la bannière existe déjà
   const editBanner = document.createElement("div");
   editBanner.classList.add("banner");
@@ -172,9 +99,13 @@ function editMode() {
     openModal(linkIcon);
   }
 }
+
+// =============================================================================
+// ========================== Fonction Mode Classique ==========================
+// =============================================================================
+
 function classicMode() {
   const divFiltreCategories = document.querySelector(".category-menu");
-  console.log(divFiltreCategories);
   if (divFiltreCategories) {
     divFiltreCategories.style.display = "block";
   }
@@ -184,62 +115,141 @@ function classicMode() {
     loginButton.textContent = "Login";
   }
 }
-// ----------------------------- Partie modale -------------------------  
 
+// =============================================================================
+// ==================== Fonction de génération des travaux =====================
+// =============================================================================
+
+// fonction qui permet de récupérer la liste des travaux depuis l'API
+function genererWorks(listWorks) {
+  // Récupération des éléments du DOM
+  const divGallery = document.querySelector('.gallery');
+  if (!divGallery) {
+    console.error("L'élément .gallery n'existe pas sur cette page.");
+    return;
+  }
+  //Effacement de l'écran et regénération de la page  
+  divGallery.innerHTML = "";
+  // Boucle qui va permettre de lister les projets
+  for (let i = 0; i < listWorks.length; i++) {
+    const figure = listWorks[i];
+    // Création de la balise figure qui acceuillera chaque projet
+    const worksElement = document.createElement('figure');
+
+    // Création des balises
+    const imageElement = document.createElement('img');
+    imageElement.src = figure.imageUrl
+    const titleElement = document.createElement('figcaption');
+    titleElement.innerText = figure.title;
+    const categoryIdElement = document.createElement("p");
+    categoryIdElement.innerText = figure.categoryId;
+
+    // Rattacher les balises
+    divGallery.appendChild(worksElement);
+    worksElement.appendChild(imageElement);
+    worksElement.appendChild(titleElement);
+    worksElement.appendChild(categoryIdElement);
+  }
+}
+
+// =============================================================================
+// ======================== Partie des boutons filtres =========================
+// =============================================================================
+
+function genererFilter(listCategory, listWorks) {
+
+  // Récupération de l'élément du DOM
+  const divFilterCategory = document.querySelector(".category-menu");
+  divFilterCategory.innerHTML = "";
+  // Création du bouton 'Tous' 
+  const buttonFilterAll = document.createElement('button');
+  buttonFilterAll.setAttribute('class', 'btn-tous button');
+  buttonFilterAll.textContent = "Tous";
+
+  divFilterCategory.appendChild(buttonFilterAll);
+
+  // Boucle pour afficher le nom de chaques catégorie de l'API
+  for (let i = 0; i < listCategory.length; i++) {
+    const buttonFilter = document.createElement('button');
+    // Récupération de l'id du bouton pour l'event Listener
+    buttonFilter.dataset.id = listCategory[i].id;
+    // console.log(buttonFilter.dataset.id) // Cela m'affiche bien mes 3 id
+    buttonFilter.setAttribute('class', 'btn-filter button');
+    buttonFilter.innerText = listCategory[i].name
+
+    divFilterCategory.appendChild(buttonFilter);
+  }
+
+  // ========== Bouton TOUS ==========
+
+  // Pour le bouton 'Tous'
+  buttonFilterAll.addEventListener('click', () => {
+    genererWorks(listWorks);
+  })
+}
+// ========== Les autres boutons ==========
+
+function addListenerFilter(listWorks) {
+  const listButton = document.querySelectorAll('.btn-filter',);
+  for (let i = 0; i < listButton.length; i++) {
+    const currentButton = listButton[i];
+    currentButton.addEventListener('click', (event) => {
+      const categoryId = parseInt(event.target.dataset.id);
+      const listWorksFilter = listWorks.filter(work => work.categoryId === categoryId);
+      genererWorks(listWorksFilter);
+    });
+  }
+}
+
+// =============================================================================
+// ----------------------------- Partie modale -------------------------  
+// =============================================================================
 // Reste à faire une fonction pour les différentes vue de la modale, une première lors du click sur 'modifier' et une deuxième qui lors du click sur le bouton 'Ajouter une photo'
 
-// Fonction d'ouverture de la modale
+// Fonction principale pour ouvrir la modale
 function openModal(linkIcon) {
-  // selection du 'href'
-  const modal = document.querySelector('#modal1');
+  const modal = document.querySelector('#modal');
+  const modalWrapper = modal.querySelector('.modal-wrapper');
+  const modalContent = document.querySelector('.modal-content');
+  const btnButtonModal = document.querySelector('.btn-modal');
+  const divGallery = document.querySelector('.gallery');
 
-  // Création du container qui contiendra la galerie
-  const modalContentContainer = document.createElement('div');
-  modalContentContainer.classList.add('modal-gallery');
+  // Ajouter le contenu à la modale
+  modalWrapper.appendChild(modalContent);
+  modalWrapper.appendChild(btnButtonModal);
 
-  // Création et ajout du bouton dans la div buttonContent
-  const buttonContent = document.createElement('div')
-  buttonContent.classList.add('btnModalContainer');
-
-  // Création du bouton 
-  const addPicture = document.createElement('button');
-  addPicture.textContent = 'Ajouter une photo';
-  addPicture.classList.add('buttonModal');
-  // Reste à ajouter un évenement au click qui renverra vers la seconde vue de la modale
-
-  buttonContent.appendChild(addPicture);
-
-  modal.querySelector('.modal-content').appendChild(modalContentContainer);
-  modal.querySelector('.modal-content').appendChild(buttonContent);
-
-  // Ajout d'un 'eventListener' au click
+  // Gestion de l'affichage de la modale
   linkIcon.addEventListener('click', function (e) {
     e.preventDefault();
-    // Clonage de la galerie des travaux
-    const divGallery = document.querySelector('.gallery')
+
+    // Nettoyage du contenu de modalContent avant d'ajouter la galerie
+    modalContent.innerHTML = '';
+
+    // Clonage de la galerie des travaux uniquement si elle existe sur la page principale
     if (divGallery) {
       const galleryClone = divGallery.cloneNode(true);
-
-      // Ajout de l'icone remove sur chaques figure clonée
       const figures = galleryClone.querySelectorAll('figure');
       figures.forEach(figure => {
         const deleteIcon = document.createElement('i');
-        deleteIcon.classList.add('fa', 'fa-trash', 'delete-icon', 'deletIcon');
+        deleteIcon.classList.add('fa', 'fa-trash', 'delete-icon');
         figure.appendChild(deleteIcon);
       });
 
-      modalContentContainer.innerHTML = '';
-      modalContentContainer.appendChild(galleryClone);
+      // Ajouter le clone de la galerie à la modalContent
+      modalContent.appendChild(galleryClone);
     }
 
-    // Reste à ajouter une condition pour la suppression des travaux lorsque l'on clique sur le bouton remove
-
+    // Afficher la modale
     modal.style.display = null;
     modal.removeAttribute('aria-hidden');
     modal.setAttribute('aria-modal', 'true');
   });
+
   // Fermeture de la modale avec le bouton 'close'
-  modal.querySelector('.modal-close').addEventListener('click', function () {
+  modal.querySelector('.js-modal-close').addEventListener('click', function () {
+    // Nettoyage du contenu de modalContent
+    modalContent.innerHTML = '';
+
     modal.style.display = 'none';
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
@@ -248,6 +258,9 @@ function openModal(linkIcon) {
   // Fermeture de la modale au click en dehors de celle-ci
   window.addEventListener('click', function (event) {
     if (event.target === modal) {
+      // Nettoyage du contenu de modalContent
+      modalContent.innerHTML = '';
+
       modal.style.display = 'none';
       modal.setAttribute('aria-hidden', 'true');
       modal.removeAttribute('aria-modal');
